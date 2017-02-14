05260dd0761be6d6c2fde85eab4f46c0269d446b
cstenkamp <cstenkamp@uos.de>
2017-02-14 17:42:39 +0100

    same as before

--- reader.py
+++ reader.py
@@ -26,6 +26,8 @@ from six.moves import xrange  # pylint: disable=redefined-builtin
 
 NAME = "test"
 
+num_steps = 100001
+
 
 ## first we create, save & load the words as indices.
 
@@ -61,7 +63,7 @@ def make_dataset():
                         allwords[word] = wordcount
                         wordcount = wordcount +1
                     else:
-                        allwords[word] = -1
+                        allwords[word] = 0
             counter = counter + 1
             if counter > HOWMANY: 
                 break
@@ -70,12 +72,9 @@ def make_dataset():
     
     forreverse = copy.deepcopy(allwords)
     forreverse = { k:v for k, v in forreverse.items() if v > 0 }
-    forreverse["UNK"] = len(forreverse)+1
+    forreverse["<UNK>"] = 0
     reverse_dictionary = dict(zip(forreverse.values(), forreverse.keys()))
-    for k,v in allwords.items():
-        if v < 0:
-            allwords[k] = len(forreverse)       
-    reverse_dictionary[len(reverse_dictionary)+1] = "UNK"    
+    reverse_dictionary[0] = "<UNK>"    
         
     with open("sets/"+NAME+".txt", encoding="utf8") as infile:        
         counter = 1
@@ -105,6 +104,8 @@ def make_dataset():
     return moviedat
 
 
+
+
 class moviedata(object):
     def __init__(self, reviews, targets, lookup, uplook, count):
         self.reviews = reviews
@@ -117,8 +118,9 @@ class moviedata(object):
         self.wordvecs = wordvecs
 
 
+#==============================================================================
 
-my_file = Path("./ratings_ohne_wordvecs.pkl")
+my_file = Path("./"+NAME+"ratings_ohne_wordvecs.pkl")
 if not my_file.is_file():
     moviedat = make_dataset()
     with open(NAME+'ratings_ohne_wordvecs.pkl', 'wb') as output:
@@ -128,223 +130,207 @@ else:
         moviedat = pickle.load(input)     
     
 
-
-
 print(""+str(moviedat.ohnum)+" different words.")
-
 rand = round(random.uniform(0,len(moviedat.targets)))
 print('Sample review', moviedat.reviews[rand][0:100], [moviedat.uplook[i] for i in moviedat.reviews[rand][0:100]])
-
-#print(moviedat.lookup)
-
-
-#TODO: Replace rare words with UNK token?
-
 
 
 ## let's get to word2vec (https://www.tensorflow.org/tutorials/word2vec/)
 #TODO: CBOW statt skip-gram, da wir nen kleines dataset haben!
 
 
-
 permutation = np.random.permutation(len(moviedat.targets))
-
 dindex = [0,0]
+
+
 
 # Step 3: Function to generate a training batch for the skip-gram model.
 def generate_batch(batch_size, num_skips, skip_window, dataset):
-  global dindex
-  assert batch_size % num_skips == 0
-  assert num_skips <= 2 * skip_window
-  batch = np.ndarray(shape=(batch_size), dtype=np.int32)
-  labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
-  span = 2 * skip_window + 1  # [ skip_window target skip_window ]
-  buffer = collections.deque(maxlen=span)
-  for _ in range(span):
-    global permutation
-    buffer.append(dataset[permutation[dindex[0]]][dindex[1]])
-    dindex[1] = dindex[1] + 1
-    if dindex[1] >= len(dataset[permutation[(dindex[0] % len(permutation))]]):
-        dindex[1] = 0
-        dindex[0] = dindex[0] + 1
-        permutation = np.random.permutation(len(moviedat.targets))      
-  for i in range(batch_size // num_skips):
-    target = skip_window  # target label at the center of the buffer
-    targets_to_avoid = [skip_window]
-    for j in range(num_skips):
-      while target in targets_to_avoid:
-        target = random.randint(0, span - 1)
-      targets_to_avoid.append(target)
-      batch[i * num_skips + j] = buffer[skip_window]
-      labels[i * num_skips + j, 0] = buffer[target]
-    buffer.append(dataset[permutation[(dindex[0] % len(permutation))]][dindex[1]])
-    dindex[1] = dindex[1] + 1
-    if dindex[1] >= len(dataset[permutation[(dindex[0] % len(permutation))]]):
-        dindex[1] = 0
-        dindex[0] = dindex[0] + 1
-        permutation = np.random.permutation(len(moviedat.targets))      
-  return batch, labels
+    global dindex
+    assert batch_size % num_skips == 0
+    assert num_skips <= 2 * skip_window
+    batch = np.ndarray(shape=(batch_size), dtype=np.int32)
+    labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
+    span = 2 * skip_window + 1  # [ skip_window target skip_window ]
+    buffer = collections.deque(maxlen=span)
+    for _ in range(span):
+      global permutation
+      buffer.append(dataset[permutation[(dindex[0] % len(permutation))]][dindex[1]])
+      dindex[1] = dindex[1] + 1
+      if dindex[1] >= len(dataset[permutation[(dindex[0] % len(permutation))]]):
+          dindex[1] = 0
+          dindex[0] = dindex[0] + 1
+          permutation = np.random.permutation(len(moviedat.targets))      
+    for i in range(batch_size // num_skips):
+      target = skip_window  # target label at the center of the buffer
+      targets_to_avoid = [skip_window]
+      for j in range(num_skips):
+        while target in targets_to_avoid:
+          target = random.randint(0, span - 1)
+        targets_to_avoid.append(target)
+        batch[i * num_skips + j] = buffer[skip_window]
+        labels[i * num_skips + j, 0] = buffer[target]
+      buffer.append(dataset[permutation[(dindex[0] % len(permutation))]][dindex[1]])
+      dindex[1] = dindex[1] + 1
+      if dindex[1] >= len(dataset[permutation[(dindex[0] % len(permutation))]]):
+          dindex[1] = 0
+          dindex[0] = dindex[0] + 1
+          permutation = np.random.permutation(len(moviedat.targets))      
+    return batch, labels
 
 batch, labels = generate_batch(batch_size=8, num_skips=2, skip_window=1, dataset=moviedat.reviews)
 for i in range(8):
   print(batch[i], moviedat.uplook[batch[i]], '->', labels[i, 0], moviedat.uplook[labels[i, 0]])
 
+
+
 #zwischen 2 generateten batches sind 1-2 wörter lücke, don't ask me why.
 
 
 
-
-
-
-
-
-
-
-
-
-
 # Step 4: Build and train a skip-gram model.
-
-batch_size = 128
-embedding_size = 128  # Dimension of the embedding vector.
-skip_window = 1       # How many words to consider left and right.
-num_skips = 2         # How many times to reuse an input to generate a label.
-
-# We pick a random validation set to sample nearest neighbors. Here we limit the
-# validation samples to the words that have a low numeric ID, which by
-# construction are also the most frequent.
-valid_size = 16     # Random set of words to evaluate similarity on.
-valid_window = 100  # Only pick dev samples in the head of the distribution.
-valid_examples = np.random.choice(valid_window, valid_size, replace=False)
-num_sampled = 64    # Number of negative examples to sample.
-
-graph = tf.Graph()
-
-with graph.as_default():
-
-  # Input data.
-  train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
-  train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
-  valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
-
-  # Ops and variables pinned to the CPU because of missing GPU implementation
-  with tf.device('/cpu:0'):
-    # Look up embeddings for inputs.
-    embeddings = tf.Variable(
-        tf.random_uniform([moviedat.ohnum, embedding_size], -1.0, 1.0))
-    embed = tf.nn.embedding_lookup(embeddings, train_inputs)
-
-    # Construct the variables for the NCE loss
-    nce_weights = tf.Variable(
-        tf.truncated_normal([moviedat.ohnum, embedding_size],
-                            stddev=1.0 / math.sqrt(embedding_size)))
-    nce_biases = tf.Variable(tf.zeros([moviedat.ohnum]))
-
-  # Compute the average NCE loss for the batch.
-  # tf.nce_loss automatically draws a new sample of the negative labels each
-  # time we evaluate the loss.
-  loss = tf.reduce_mean(
-      tf.nn.nce_loss(weights=nce_weights,
-                     biases=nce_biases,
-                     labels=train_labels,
-                     inputs=embed,
-                     num_sampled=num_sampled,
-                     num_classes=moviedat.ohnum))
-
-  # Construct the SGD optimizer using a learning rate of 1.0.
-  optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
-
-  # Compute the cosine similarity between minibatch examples and all embeddings.
-  norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
-  normalized_embeddings = embeddings / norm
-  valid_embeddings = tf.nn.embedding_lookup(
-      normalized_embeddings, valid_dataset)
-  similarity = tf.matmul(
-      valid_embeddings, normalized_embeddings, transpose_b=True)
-
-  # Add variable initializer.
-  init = tf.global_variables_initializer()
-
-# Step 5: Begin training.
-num_steps = 100001
-
-with tf.Session(graph=graph) as session:
-  # We must initialize all variables before we use them.
-  init.run()
-  print("Initialized")
-
-  average_loss = 0
-  for step in xrange(num_steps):
-    batch_inputs, batch_labels = generate_batch(
-        batch_size, num_skips, skip_window, dataset=moviedat.reviews)
-    feed_dict = {train_inputs: batch_inputs, train_labels: batch_labels}
-
-
-    # We perform one update step by evaluating the optimizer op (including it
-    # in the list of returned values for session.run()
-    _, loss_val = session.run([optimizer, loss], feed_dict=feed_dict)
-    average_loss += loss_val
-
-    if step % 2000 == 0:
-      if step > 0:
-        average_loss /= 2000
-      # The average loss is an estimate of the loss over the last 2000 batches.
-      print("Average loss at step ", step, ": ", average_loss)
+def perform_word2vec(dataset):
+    batch_size = 128
+    embedding_size = 128  # Dimension of the embedding vector.
+    skip_window = 1       # How many words to consider left and right.
+    num_skips = 2         # How many times to reuse an input to generate a label.
+    
+    # We pick a random validation set to sample nearest neighbors. Here we limit the
+    # validation samples to the words that have a low numeric ID, which by
+    # construction are also the most frequent.
+    valid_size = 16     # Random set of words to evaluate similarity on.
+    valid_window = 100  # Only pick dev samples in the head of the distribution.
+    valid_examples = np.random.choice(valid_window, valid_size, replace=False)
+    num_sampled = 64    # Number of negative examples to sample.
+    
+    graph = tf.Graph()
+    with graph.as_default():
+      # Input data.
+      train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
+      train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
+      valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+    
+      # Ops and variables pinned to the CPU because of missing GPU implementation
+      with tf.device('/cpu:0'):
+        # Look up embeddings for inputs.
+        embeddings = tf.Variable(
+            tf.random_uniform([moviedat.ohnum, embedding_size], -1.0, 1.0))
+        embed = tf.nn.embedding_lookup(embeddings, train_inputs)
+    
+        # Construct the variables for the NCE loss
+        nce_weights = tf.Variable(
+            tf.truncated_normal([dataset.ohnum, embedding_size],
+                                stddev=1.0 / math.sqrt(embedding_size)))
+        nce_biases = tf.Variable(tf.zeros([dataset.ohnum]))
+    
+      # Compute the average NCE loss for the batch.
+      # tf.nce_loss automatically draws a new sample of the negative labels each
+      # time we evaluate the loss.
+      loss = tf.reduce_mean(
+          tf.nn.nce_loss(weights=nce_weights,
+                         biases=nce_biases,
+                         labels=train_labels,
+                         inputs=embed,
+                         num_sampled=num_sampled,
+                         num_classes=dataset.ohnum))
+    
+      # Construct the SGD optimizer using a learning rate of 1.0.
+      optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
+    
+      # Compute the cosine similarity between minibatch examples and all embeddings.
+      norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
+      normalized_embeddings = embeddings / norm
+      valid_embeddings = tf.nn.embedding_lookup(
+          normalized_embeddings, valid_dataset)
+      similarity = tf.matmul(
+          valid_embeddings, normalized_embeddings, transpose_b=True)
+    
+      # Add variable initializer.
+      init = tf.global_variables_initializer()
+    
+    
+    # Step 5: Begin training.
+    global num_steps
+    with tf.Session(graph=graph) as session:
+      # We must initialize all variables before we use them.
+      init.run()
+      print("Initialized")    
       average_loss = 0
+      for step in xrange(num_steps):
+        batch_inputs, batch_labels = generate_batch(
+            batch_size, num_skips, skip_window, dataset=dataset.reviews)
+        feed_dict = {train_inputs: batch_inputs, train_labels: batch_labels}
+    
+        # We perform one update step by evaluating the optimizer op (including it
+        # in the list of returned values for session.run()
+        _, loss_val = session.run([optimizer, loss], feed_dict=feed_dict)
+        average_loss += loss_val
+    
+        if step % 2000 == 0:
+          if step > 0:
+            average_loss /= 2000
+          # The average loss is an estimate of the loss over the last 2000 batches.
+          print("Average loss at step ", step, ": ", average_loss)
+          average_loss = 0
+    
+        # Note that this is expensive (~20% slowdown if computed every 500 steps)
+        if step % 10000 == 0:
+          sim = similarity.eval()
+          for i in xrange(valid_size):
+            valid_word = dataset.uplook[valid_examples[i]]
+            top_k = 8  # number of nearest neighbors
+            nearest = (-sim[i, :]).argsort()[1:top_k + 1]
+            log_str = "Nearest to %s:" % valid_word
+            for k in xrange(top_k):
+              close_word = dataset.uplook[nearest[k]]
+              log_str = "%s %s," % (log_str, close_word)
+            print(log_str)
+      final_embeddings = normalized_embeddings.eval()
+    return final_embeddings
+    
 
-    # Note that this is expensive (~20% slowdown if computed every 500 steps)
-    if step % 10000 == 0:
-      sim = similarity.eval()
-      for i in xrange(valid_size):
-        valid_word = moviedat.uplook[valid_examples[i]]
-        top_k = 8  # number of nearest neighbors
-        nearest = (-sim[i, :]).argsort()[1:top_k + 1]
-        log_str = "Nearest to %s:" % valid_word
-        for k in xrange(top_k):
-          close_word = moviedat.uplook[nearest[k]]
-          log_str = "%s %s," % (log_str, close_word)
-        print(log_str)
-  final_embeddings = normalized_embeddings.eval()
 
-  moviedat.add_wordvecs(final_embeddings)
-  with open(NAME+'ratings_mit_wordvecs.pkl', 'wb') as output:
-      pickle.dump(moviedat, output, pickle.HIGHEST_PROTOCOL)  
+final_embeddings = perform_word2vec(moviedat)
+moviedat.add_wordvectors(final_embeddings)
+with open(NAME+'ratings_mit_wordvecs.pkl', 'wb') as output:
+    pickle.dump(moviedat, output, pickle.HIGHEST_PROTOCOL)  
+
+
+
 
 
 # Step 6: Visualize the embeddings.
 
-
-def plot_with_labels(low_dim_embs, labels, filename='tsne.png'):
-  assert low_dim_embs.shape[0] >= len(labels), "More labels than embeddings"
-  plt.figure(figsize=(18, 18))  # in inches
-  for i, label in enumerate(labels):
-    x, y = low_dim_embs[i, :]
-    plt.scatter(x, y)
-    plt.annotate(label,
-                 xy=(x, y),
-                 xytext=(5, 2),
-                 textcoords='offset points',
-                 ha='right',
-                 va='bottom')
-
-  plt.savefig(filename)
-
-try:
-  from sklearn.manifold import TSNE
-  import matplotlib.pyplot as plt
-
-  tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
-  plot_only = 500
-  low_dim_embs = tsne.fit_transform(final_embeddings[:plot_only, :])
-  labels = [moviedat.uplook[i] for i in xrange(plot_only)]
-  plot_with_labels(low_dim_embs, labels)
-
-except ImportError:
-  print("Please install sklearn, matplotlib, and scipy to visualize embeddings.")
+def plot_tsne(final_embeddings, dataset):
+    def plot_with_labels(low_dim_embs, labels, filename='tsne.png'):
+        assert low_dim_embs.shape[0] >= len(labels), "More labels than embeddings"
+        plt.figure(figsize=(18, 18))  # in inches
+        for i, label in enumerate(labels):
+          x, y = low_dim_embs[i, :]
+          plt.scatter(x, y)
+          plt.annotate(label,
+                       xy=(x, y),
+                       xytext=(5, 2),
+                       textcoords='offset points',
+                       ha='right',
+                       va='bottom')
+        plt.savefig(filename)
+    try:
+      from sklearn.manifold import TSNE
+      import matplotlib.pyplot as plt
+      tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+      plot_only = 500
+      low_dim_embs = tsne.fit_transform(final_embeddings[:plot_only, :])
+            
+      labels = [dataset.uplook[i] for i in xrange(plot_only)]
+      plot_with_labels(low_dim_embs, labels)
+    except ImportError:
+      print("Please install sklearn, matplotlib, and scipy to visualize embeddings.")
 
 
 
 
+plot_tsne(final_embeddings, moviedat)
 
 
 
